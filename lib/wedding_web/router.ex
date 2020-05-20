7fdc620202e4a7598eb75cb6913083f265f741e5
Tomasz Tomczyk <me@tomasztomczyk.com>
2020-05-20 22:49:22 +0100

    Initial setup

---
+++ lib/wedding_web/router.ex
@@ -0,0 +1,27 @@
+defmodule WeddingWeb.Router do
+  use WeddingWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {WeddingWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", WeddingWeb do
+    pipe_through :browser
+
+    live "/", PageLive, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", WeddingWeb do
+  #   pipe_through :api
+  # end
+end
